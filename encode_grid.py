13a581e0215c9f2f0bc3c57c56d419d74351b172
AnanyaAppan <ananya.appan@gmail.com>
2019-11-15 18:51:17 +0530

    tried to make grids using bash

--- encode_grid.py
+++ encode_grid.py
@@ -13,18 +13,20 @@ spark = SparkSession \
 df = spark.read.load(filename,format="csv", sep=",", inferSchema="true", header="false").toDF("Index","busId" , "latitude", "longitude", "angle", "speed", "timestamp")
 # Displays the content of the DataFrame to stdout
 df.printSchema()
-length_lat = 0.032
-length_long = 0.043
-i = 1000
-j = 1000
+length_lat = 0.032/2
+length_long = 0.043/2
+# i rows 42
+i = 0
+# j columns 34
+j = 0
 lat = 12.66 
 long = 77.27 
 
 while(lat < 13.32):
     while(long < 78):
-        print("nkdjnjs  ", lat+length_lat)
-        df_filtered = df.filter((df.latitude > lat) & (df.latitude < lat+length_lat) & (df.longitude > long) & (df.longitude < long+length_long))
-        df_filtered.toPandas().to_csv('../../BMTC/gridData/grid_'+str(i)+'_'+str(j)+'.csv', mode='a', header=False, index=False)
+        if(i==21 and j==17):
+            df_filtered = df.filter((df.latitude > lat) & (df.latitude < lat+length_lat) & (df.longitude > long) & (df.longitude < long+length_long))
+            df_filtered.toPandas().to_csv('../../BMTC/gridData/grid_'+str(i)+'_'+str(j)+'.csv', mode='a', header=False, index=False)
         long += length_long
         j += 1
     i += 1
