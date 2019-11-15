9fb77e9f3233415278cc2b7acd79291e58a4c52b
swasti <swastishreya.mishra@iiitb.org>
2019-11-16 01:31:56 +0530

    regression model

---
+++ reg_mod_grid.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import normalize
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+
+filepath = ""
+grid = pd.read_csv(filepath)
+
