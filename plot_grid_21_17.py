6704a162ed3c199dad06b888adf8e0990e14de4a
AnanyaAppan <ananya.appan@gmail.com>
2019-11-16 09:49:34 +0530

    plotted time vs angle

--- plot_grid_21_17.py
+++ plot_grid_21_17.py
@@ -4,16 +4,17 @@ import numpy as np
 import sys
 
 # filename = "../../BMTC/sorted_data/" + sys.argv[1]
-filename = "grid_21_17_zero_removed.csv"
-df = pd.read_csv(filename, names = ["Index", "busId" , "latitude", "longitude", "angle", "speed", "timestamp"])
+filename = "encoded_grid_21_17.csv"
+df = pd.read_csv(filename, names = ["Index", "busId" , "latitude", "longitude", "angle", "speed", "timestamp","encodedTime"])
 
 lat = list(map(float,df.latitude.tolist()[1:]))
 long = list(map(float,df.longitude.tolist()[1:]))
 angle = list(map(float,df.angle.tolist()[1:]))
 speed = list(map(float,df.speed.tolist()[1:]))
+encodedTime = list(map(float,df.encodedTime.tolist()[1:]))
 
 
-plt.scatter(speed,angle,marker='.')
-# plt.plot(speed,angle,linewidth=2.0)
+# plt.scatter(angle,speed,marker='.')
+plt.scatter(encodedTime,speed,marker='.')
 
 plt.show()
