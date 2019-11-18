ca9d3d0d4c12ffcae0f2626ca2f968a1b0a3aaae
AnanyaAppan <ananya.appan@gmail.com>
2019-11-19 00:41:08 +0530

    pulling again :p

--- encode_data.py
+++ encode_data.py
@@ -1,8 +1,7 @@
 import pandas as pd 
 
 
-
-chunk = pd.read_csv("grid_21_17_zero_removed.csv", names = ["Index","busId" , "latitude", "longitude", "angle", "speed", "timestamp"])
+chunk = pd.read_csv("grid_21_17_zero_removed.csv", names = ["index","busId" , "latitude", "longitude", "angle", "speed", "timestamp","gridNum"])
 df = pd.DataFrame(chunk)
 
 df["time"] = [int(var[11:13])*60 + int(var[14:16]) for var in df["timestamp"]]
