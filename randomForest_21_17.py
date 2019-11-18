e4f4bf255b79d7210a6d8fe4014c8fc1807109fa
swasti <swastishreya.mishra@iiitb.org>
2019-11-18 20:15:59 +0530

    Modified polynomial regression and added random forest

---
+++ randomForest_21_17.py
@@ -0,0 +1,31 @@
+import numpy as np 
+import pandas as pd
+import matplotlib.pyplot as plt 
+
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.preprocessing import StandardScaler
+from sklearn import metrics
+
+df = pd.read_csv('encoded_grid_21_17.csv', header=None, nrows = 10000)
+
+train = df.sample(frac=0.8,random_state=42) #random state is a seed value
+test = df.drop(train.index)
+
+X_train = train.iloc[:,[2,3,4,7]].values
+y_train = train.iloc[:,5].values
+
+X_test = test.iloc[:,[2,3,4,7]].values
+y_test = test.iloc[:,5].values
+
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+regressor = RandomForestRegressor(n_estimators=100, random_state=42)
+regressor.fit(X_train, y_train)
+y_pred = regressor.predict(X_test)
+
+print('Mean Absolute Error:', metrics.mean_absolute_error(y_test, y_pred))
+print('Mean Squared Error:', metrics.mean_squared_error(y_test, y_pred))
+print('Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
+
--- regression_21_17.py
+++ regression_21_17.py
@@ -9,32 +9,37 @@ from sklearn.metrics import mean_squared_error, r2_score
 
 df = pd.read_csv('encoded_grid_21_17.csv', header=None, nrows = 100000)
 
-train=df.sample(frac=0.8,random_state=200) #random state is a seed value
+train=df.sample(frac=0.8,random_state=42) #random state is a seed value
 test=df.drop(train.index)
 
 X = train.iloc[:,[2,3,4,7]].values
 y = train.iloc[:,5].values
-print(X.shape)
-print(y.shape)
+X_test = test.iloc[:,[2,3,4,7]].values
+y_test = test.iloc[:,5].values
 
-poly = PolynomialFeatures(degree = 7) 
+poly = PolynomialFeatures(degree = 5) 
 X_poly = poly.fit_transform(X)
+X_poly_test = poly.fit_transform(X_test)
 X_poly = StandardScaler().fit_transform(X_poly)
+X_test = StandardScaler().fit_transform(X_poly_test)
 print(X_poly.shape)
+print(X_test.shape)
 
 comp=20
 cols = ['principal component ' + str(x) for x in range(1,comp+1)]
 pca = PCA(n_components=comp)
 principalComponents = pca.fit_transform(X_poly)
+principalComponentsTest = pca.transform(X_test)
 X_poly = pd.DataFrame(data = principalComponents , columns = cols)
+X_test = pd.DataFrame(data = principalComponentsTest , columns = cols)
   
 poly.fit(X_poly, y) 
 lin2 = LinearRegression() 
 lin2.fit(X_poly, y) 
-y_poly_pred = lin2.predict(X_poly)
+y_poly_pred = lin2.predict(X_test)
 
-rmse = np.sqrt(mean_squared_error(y,y_poly_pred))
-r2 = r2_score(y,y_poly_pred)
+rmse = np.sqrt(mean_squared_error(y_test,y_poly_pred))
+r2 = r2_score(y_test,y_poly_pred)
 print(rmse)
 print(r2)
 
